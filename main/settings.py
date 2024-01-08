23e97ef5d1357014fc14ac7a34424adc9629f13a
VivekPatil81400 <59966933+VivekPatil81400@users.noreply.github.com>
2024-01-08 20:09:15 +0100

    notification setup gmail

--- main/settings.py
+++ main/settings.py
@@ -138,3 +138,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DEBUG = True
+
+# settings.py
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your-gmail-email@gmail.com'
+EMAIL_HOST_PASSWORD = 'your-gmail-password'
